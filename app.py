67961bb8f5c876fba71ec0e51e1737549e0b8c6f
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-11-21 11:46:40 +0700

    init

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def getDetailPlace(Place_Id) :
     })
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))  
+    app.run(host="0.0.0.0", port=port)
